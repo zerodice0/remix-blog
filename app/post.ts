b92e880b5d76661046f7e2bc2028e6d9b19d192d
zerodice0 <zerodice0@gmail.com>
2021-12-20 19:57:37 +0900

    [fix] - remove global variable 'postsPath'. - add PostMarkdownAttributes. - add function 'isValidPostAttributes'. - fix getPosts function to using invariant, marked.

--- app/post.ts
+++ app/post.ts
@@ -1,33 +1,36 @@
 import path from "path";
 import fs from "fs/promises";
 import parseFrontMatter from "front-matter";
+import invariant from "tiny-invariant";
+import { marked } from "marked";
 
 export type Post = {
   slug: string;
   title: string;
 }
 
-const postsPath = path.join(__dirname, "..", "posts");
+export type PostMarkdownAttributes = {
+  title: string;
+};
 
-export async function getPosts() {
-  const dir = await fs.readdir(postsPath);
+function isValidPostAttributes(
+  attributes: any
+): attributes is PostMarkdownAttributes {
+  return attributes?.title;
+}
+
+export async function getPosts(slug: string) {
+  const postsPath = path.join(__dirname, "..", "posts");
+  const filePath = path.join(postsPath, slug + ".md");
+  const file = await fs.readFile(filePath);
+  const { attributes, body } = parseFrontMatter(file.toString());
   
-  return Promise.all(
-    dir.map(async filename => (
-      const file = await fs.readFile(
-        path.join(postsPath, filename)
-      );
-
-      const { attributes } = parseFrontMatter(
-        file.toString()
-      );
-
-      return {
-        slug: filename.replace(/\.md$/. ""),
-        title: attributes.title
-      }
-    ))
+  invariant(
+    isValidPostAttributes(attributes),
+    `Post ${filePath} is missing attributes`
   );
 
-  return posts;
+  const html = marked(body);
+
+  return { slug, html, title: attributes.title };
 }
