6bb0b3b4653c2a552b9766515c996021053e7659
zerodice0 <zerodice0@gmail.com>
2021-12-20 20:49:46 +0900

    [fix] - revoke getPost function from app/post.ts. - display post list on posts/index.tsx.

--- app/post.ts
+++ app/post.ts
@@ -19,7 +19,34 @@ function isValidPostAttributes(
   return attributes?.title;
 }
 
-export async function getPosts(slug: string) {
+export async function getPosts() {
+  const postsPath = path.join(__dirname, "..", "posts");
+  const dir = await fs.readdir(postsPath);
+
+  return Promise.all(
+    dir.map(async filename => {
+      const file = await fs.readFile(
+        path.join(postsPath, filename)
+      );
+
+      const { attributes } = parseFrontMatter(
+        file.toString()
+      );
+
+      invariant(
+        isValidPostAttributes(attributes),
+        `${filename} has bad meta data!`
+      );
+
+      return {
+        slug: filename.replace(/\.md$/, ""),
+        title: attributes.title
+      };
+    })
+  );
+}
+
+export async function getPost(slug: string) {
   const postsPath = path.join(__dirname, "..", "posts");
   const filePath = path.join(postsPath, slug + ".md");
   const file = await fs.readFile(filePath);
--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,12 +1,12 @@
 import { useLoaderData } from "remix";
 import type { LoaderFunction } from "remix";
-import { getPosts } from "~/post";
+import { getPost } from "~/post";
 import invariant from "tiny-invariant";
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.slug, "expected params.slug");
 
-  return getPosts(params.slug);
+  return getPost(params.slug);
 };
 
 export default function PostSlug() {
